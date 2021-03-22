5b19d444ec6018f39f59e55da913e415bd37d4cf
Jonas Bernardino <jonas.bernardino@dcx.ufpb.br>
2021-03-22 16:18:19 -0300

    Criando projeto

---
+++ src/main/java/com/jonas/cliente/model/entity/Cliente.java
@@ -0,0 +1,26 @@
+package com.jonas.cliente.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    private LocalDate dataCadastro;
+}
---
+++ src/main/java/com/jonas/cliente/model/entity/Servico.java
@@ -0,0 +1,28 @@
+package com.jonas.cliente.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn
+    private Cliente cliente;
+
+    private BigDecimal valor;
+}
